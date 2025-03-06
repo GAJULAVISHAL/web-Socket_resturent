4d18ee5f531b61ab1c0dc2f365f2f699d183d7c8
G.Vishal <vishalgajula0709@gmail.com>
2025-03-07 00:26:16 +0530

    webSocket created

---
+++ dist/index.js
@@ -0,0 +1,24 @@
+"use strict";
+var __importDefault = (this && this.__importDefault) || function (mod) {
+    return (mod && mod.__esModule) ? mod : { "default": mod };
+};
+Object.defineProperty(exports, "__esModule", { value: true });
+const express_1 = __importDefault(require("express"));
+const ws_1 = require("ws");
+const app = (0, express_1.default)();
+const PORT = process.env.PORT || 8080;
+const httpServer = app.listen(PORT, () => {
+    console.log(`🚀 Server is running on port ${PORT}`);
+    console.log(`📡 WebSocket server is ready`);
+});
+const wss = new ws_1.WebSocketServer({ server: httpServer });
+wss.on('connection', function connection(ws) {
+    ws.on('error', console.error);
+    ws.on('message', function message(data, isBinary) {
+        wss.clients.forEach(function each(client) {
+            if (client.readyState === ws_1.WebSocket.OPEN) {
+                client.send(data, { binary: isBinary });
+            }
+        });
+    });
+});
---
+++ src/index.ts
@@ -0,0 +1,25 @@
+import express from 'express'
+import { WebSocketServer, WebSocket } from 'ws'
+
+const app = express()
+
+const PORT = process.env.PORT || 8080;
+
+const httpServer = app.listen(PORT, () => {
+  console.log(`🚀 Server is running on port ${PORT}`);
+  console.log(`📡 WebSocket server is ready`);
+})
+
+const wss = new WebSocketServer({ server: httpServer });
+
+wss.on('connection', function connection(ws) {
+  ws.on('error', console.error);
+
+  ws.on('message', function message(data, isBinary) {
+    wss.clients.forEach(function each(client) {
+      if (client.readyState === WebSocket.OPEN) {
+        client.send(data, { binary: isBinary });
+      }
+    });
+  });
+});
